9b0b0cacb1b735bf12f3e5ddba090293585d81ce
zamanAEL <assad.zaman@gmail.com>
2015-12-05 14:53:32 +0600

    add javascript

---
+++ scripts/JavaScript.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+    addStyleSheet('style_sheet/my_style.css');
+}
+
+function addStyleSheet(href) {
+    var head = document.head
+  , link = document.createElement('link');
+
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.href = href;
+
+    head.appendChild(link)
+}
