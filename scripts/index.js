0b8b304adc0a744eec29b58705d678e2fa8e133c
zamanAEL <assad.zaman@gmail.com>
2015-12-08 12:41:45 +0600

    js

--- scripts/index.js
+++ scripts/index.js
@@ -12,40 +12,38 @@ function replaceToken() {
 
 
 
-    addText("userId", "11009(Naim Islam)"); //USER_ID
-    addText("agentName", "Raqibul Alom");
-    addText("agentPoint", "Kashipur Bazar");
-    addText("boothAddress", "Vairab Chowdhurir Hath, Sonagazi");
-    addText("outletName", "Vairab Chowdhurir Hath, Sonagazi");
-
-    addText("accountName", "Mohammad Karim");
+    addText("userId", "11009(Naim Islam)"); //#USER_ID
+    addText("agentName", "Raqibul Alom"); //#AGENT_NAME
+    addText("agentPoint", "Kashipur Bazar"); //#AGENT_POINT
+    addText("boothAddress", "Vairab Chowdhurir Hath, Sonagazi"); //#BOOTH_ADDRESS
+    addText("outletName", "Vairab Chowdhurir Hath, Sonagazi"); //#OUTLATE_NAME
+    addText("accountName", "Mohammad Karim"); //#ACCOUNT_NAME
     addText("accountNo", "100-5000001-001"); //Account Number will be 13 characters //#ACCOUNT_NO
-    addText("accountOpeningDate", "30-NOV-2015");
+    addText("accountOpeningDate", "30-NOV-2015"); //#ACCOUNT_OPENING_DATE
+    addText("cashDeposit", "Cash Deposit"); //#CASH_DEPOSIT
+    addText("cashWithdraw", "Cash Withdraw"); //#CASH_WITHDRAW
+    addText("currentAccountType", "Current Account"); //#CURRENT_ACCOUNT_TYPE
+    addText("district", "Feni"); //#DISTRICT
+    addText("withdrawAmount", "15000Tk. Fifteen Thousand Only. "); //#WITHDRAW_AMOUNT
+    addText("depositAmount", "1000Tk. One Thousand Only. "); //#DEPOSIT_AMOUNT
+    addText("endDate", "30-NOV-2027"); //#END_DATE
+    addText("expireDate", "30-NOV-2027"); //#EXPAIRE_DATE
+    addText("idNo", "CB5000001"); //ID will be 7 characters #ID_NO
+    addText("initialDeposit", "Tk. 5000"); //#INITIAL_DEPOSIT
+    addText("linkAccountNumber", "100-5000001-001"); //#LINK_ACCOUNT_NUMBER
+    addText("mobileNo", "01799123456"); //#MOBILE_NO
+    addText("name", "Mohammad Karim"); //#NAME
+    addText("printDate", "30-NOV-2015 11:15:15AM"); // Print Date will be Timestamp #PRINT_DATE
 
-    addText("cashDeposit", "Cash Deposit"); //TRANSACTION_TYPE
-    addText("cashWithdraw", "Cash Withdraw"); //TRANSACTION_TYPE
-    addText("currentAccountType", "Current Account");
+    addText("requestId", "736373736"); //#REQUEST_ID
+    addText("savingsAccountType", "Savings Account"); //#SAVINGS_ACCOUNT_TYPE
+    addText("subDistrict", "Sonagazi"); //#SUB_DISTRICT
+    addText("transactionDate", "30-NOV-201"); //#TRANSACTION_DATE
+    addText("transactionId", "TR025552"); //#TRANSACTION_ID
 
-    addText("district", "Feni");
-    addText("withdrawAmount", "15000Tk. Fifteen Thousand Only. "); //WITHDRAW_AMOUNT
-    addText("depositAmount", "1000Tk. One Thousand Only. "); //DEPOSIT_AMOUNT
-    addText("endDate", "30-NOV-2027");
-    addText("idNo", "CB5000001"); //ID will be 7 characters
-    addText("initialDeposit", "Tk. 5000");
-    addText("linkAccountNumber", "100-5000001-001");
-    addText("mobileNo", "01799123456");
-    addText("name", "Mohammad Karim");
-    addText("printDate", "30-NOV-2015 11:15:15AM"); // Print Date will be Timestamp
-
-    addText("requestId", "736373736"); //REQUEST_ID
-    addText("savingsAccountType", "Savings Account");
-    addText("subDistrict", "Sonagazi");
-    addText("transactionDate", "30-NOV-201"); //TRANSACTION_DATE
-    addText("transactionId", "TR025552"); //TRANSACTION_ID
-    
-    addText("timeSpan", "১২ বছর");
-    addText("village", "Sujapur");
-    addText("union", "Vhairab Chowdhurir Hat");
+    addText("timeSpan", "১২ বছর"); //#TIMESPAN
+    addText("village", "Sujapur"); //#VILLAGE
+    addText("union", "Vhairab Chowdhurir Hat"); //#UNION
 }
 
 function addImage(elementId, path) {
