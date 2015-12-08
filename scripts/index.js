47a24645892e4bda6b4ec073492a966016fb7c51
titucs <titucs@yahoo.com>
2015-12-08 11:02:24 +0600

    test

--- scripts/index.js
+++ scripts/index.js
@@ -16,13 +16,19 @@ function replaceToken() {
     addText("agentName", "Raqibul Alom");
     addText("agentPoint", "Kashipur Bazar");
     addText("boothAddress", "Vairab Chowdhurir Hath, Sonagazi");
+    addText("outletName", "Vairab Chowdhurir Hath, Sonagazi");
 
     addText("accountName", "Mohammad Karim");
     addText("accountNo", "100-5000001-001"); //Account Number will be 13 characters //#ACCOUNT_NO
     addText("accountOpeningDate", "30-NOV-2015");
-   
+
+    addText("cashDeposit", "Cash Deposit"); //TRANSACTION_TYPE
+    addText("cashWithdraw", "Cash Withdraw"); //TRANSACTION_TYPE
     addText("currentAccountType", "Current Account");
+
     addText("district", "Feni");
+    addText("withdrawAmount", "15000Tk. Fifteen Thousand Only. "); //WITHDRAW_AMOUNT
+    addText("depositAmount", "1000Tk. One Thousand Only. "); //DEPOSIT_AMOUNT
     addText("endDate", "30-NOV-2027");
     addText("idNo", "CB5000001"); //ID will be 7 characters
     addText("initialDeposit", "Tk. 5000");
@@ -30,9 +36,13 @@ function replaceToken() {
     addText("mobileNo", "01799123456");
     addText("name", "Mohammad Karim");
     addText("printDate", "30-NOV-2015 11:15:15AM"); // Print Date will be Timestamp
+
+    addText("requestID", "736373736"); //REQUEST_ID
     addText("savingsAccountType", "Savings Account");
     addText("subDistrict", "Sonagazi");
     addText("transactionDate", "30-NOV-201"); //TRANSACTION_DATE
+    addText("transactionId", "TR025552"); //TRANSACTION_ID
+    
     addText("timeSpan", "১২ বছর");
     addText("village", "Sujapur");
     addText("union", "Vhairab Chowdhurir Hat");
