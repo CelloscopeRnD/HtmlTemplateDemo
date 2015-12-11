8506cef72e570309aa1b2827ba7355c13646c4a4
zamanAEL <assad.zaman@gmail.com>
2015-12-11 16:14:42 +0600

    test

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@
 }
 
 function replaceToken() {
-    addImage("agent_banking_logo", "images/agent_bangking_logo.bmp");
-    addImage("logo", "images/logo.png");
+    addImage("agent_banking_logo", "images/AGENT-BANKING-LOGO_Small.png");
+    addImage("logo", "images/CityBankLogo.png");
     addImage("customerPhoto", "images/photo.png");
     addImage("qr", "images/qr.png");
 
