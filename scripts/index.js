fee1f4d54972ba7cb754b09db4e7b52705679c0e
zamanAEL <assad.zaman@gmail.com>
2016-02-04 14:18:54 +0600

    refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,86 +1,49 @@
-﻿var receipts = {
-    ACCOUNT_BALANCE: 1,
-    CASH_DEPOSIT: 2,
-    CASH_WITHDRAW: 3,
-    DPS_ACCOUNT: 4,
-    FIXED_DEPOSIT: 5,
-    FUND_TRANSFER: 6,
-    SAVINGS_ACCOUNT: 7,
-    ATM_DEBIT_CARD_REQUEST: 8,
-    ATM_DEBIT_CARD_DELIVERY: 9,
-    ATM_DEBIT_CARD_CANCEL: 10,
-    CHEQUE_BOOK_REQUISITION: 11,
-    CHEQUE_BOOK_DELIVERY: 12,
-    CHEQUE_STOP_PAYMENT_SINGLE: 13,
-    CHEQUE_STOP_PAYMENT_RANGE: 14,
-    CHEQUE_STOP_PAYMENT_SINGLE_CANCEL: 15,
-    CHEQUE_STOP_PAYMENT_RANGE_CANCEL: 16
-};
+﻿//var receipts = {
+//    ACCOUNT_BALANCE: 1,
+//    CASH_DEPOSIT: 2,
+//    CASH_WITHDRAW: 3,
+//    DPS_ACCOUNT: 4,
+//    FIXED_DEPOSIT: 5,
+//    FUND_TRANSFER: 6,
+//    SAVINGS_ACCOUNT: 7,
+//    ATM_DEBIT_CARD_REQUEST: 8,
+//    ATM_DEBIT_CARD_DELIVERY: 9,
+//    ATM_DEBIT_CARD_CANCEL: 10,
+//    CHEQUE_BOOK_REQUISITION: 11,
+//    CHEQUE_BOOK_DELIVERY: 12,
+//    CHEQUE_STOP_PAYMENT_SINGLE: 13,
+//    CHEQUE_STOP_PAYMENT_RANGE: 14,
+//    CHEQUE_STOP_PAYMENT_SINGLE_CANCEL: 15,
+//    CHEQUE_STOP_PAYMENT_RANGE_CANCEL: 16
+//};
+
+var receipts = [
+    'ACCOUNT_BALANCE',
+    'CASH_DEPOSIT',
+    'CASH_WITHDRAW',
+    'DPS_ACCOUNT',
+    'FIXED_DEPOSIT',
+    'FUND_TRANSFER',
+    'SAVINGS_ACCOUNT',
+    'ATM_DEBIT_CARD_REQUEST',
+    'ATM_DEBIT_CARD_DELIVERY',
+    'ATM_DEBIT_CARD_CANCEL',
+    'CHEQUE_BOOK_REQUISITION',
+    'CHEQUE_BOOK_DELIVERY',
+    'CHEQUE_STOP_PAYMENT_SINGLE',
+    'CHEQUE_STOP_PAYMENT_RANGE',
+    'CHEQUE_STOP_PAYMENT_SINGLE_CANCEL',
+    'CHEQUE_STOP_PAYMENT_RANGE_CANCEL'];
 
 window.onload = function myfunction() {
-
-    document.getElementById("accountBalance").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.ACCOUNT_BALANCE;
+    var formSelect = document.getElementById('formSelect');
+    for (var i = 0; i < receipts.length; i++) {
+        var option = document.createElement('option');
+        option.value = i;
+        option.innerHTML = receipts[i];
+        formSelect.appendChild(option);
     }
-
-    document.getElementById("cashDeposit").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.CASH_DEPOSIT;
-    }
-
-    document.getElementById("cashWithdraw").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.CASH_WITHDRAW;
-    }
-
-    document.getElementById("dpsAccount").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.DPS_ACCOUNT;
-    }
-
-    document.getElementById("fixedDeposit").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.FIXED_DEPOSIT;
-    }
-
-    document.getElementById("fundTransfer").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.FUND_TRANSFER;
-    }
-
-    document.getElementById("savingsAccount").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.SAVINGS_ACCOUNT;
-    }
-
-
-    document.getElementById("atmDebitCardRequest").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.ATM_DEBIT_CARD_REQUEST;
-    }
-
-    document.getElementById("atmDebitCardDelivery").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.ATM_DEBIT_CARD_DELIVERY;
-    }
-
-    document.getElementById("atmDebitCardCancel").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.ATM_DEBIT_CARD_CANCEL;
-    }
-
-    document.getElementById("chequeBookRequisition").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.CHEQUE_BOOK_REQUISITION;
-    }
-
-    document.getElementById("chequeBookDelivery").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.CHEQUE_BOOK_DELIVERY;
-    }
-
-    document.getElementById("chequeStopPaymentSingle").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.CHEQUE_STOP_PAYMENT_SINGLE;
-    }
-
-    document.getElementById("chequeStopPaymentRange").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.CHEQUE_STOP_PAYMENT_RANGE;
-    }
-
-    document.getElementById("chequeStopPaymentSingleCancel").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.CHEQUE_STOP_PAYMENT_SINGLE_CANCEL;
-    }
-
-    document.getElementById("chequeStopPaymentRangeCancel").onclick = function () {
-        window.location.href = "../template1.html?" + receipts.CHEQUE_STOP_PAYMENT_RANGE_CANCEL;
+    document.getElementById("go").onclick = function () {
+        window.location.href = "../template1.html?" + (parseInt(formSelect.value) + 1);
     }
 }
